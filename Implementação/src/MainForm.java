4acd7b2309966fbacd74fc3be0ea59f431dc1f02
GuilhermeTeixeiraL <guiteixeira423@gmail.com>
2024-06-26 01:41:43 +0100

    2.0

--- Implementação/src/MainForm.java
+++ Implementação/src/MainForm.java
@@ -1,5 +1,5 @@
-import view.FuncionarioForm;
-import view.LivroForm;
+import view.*;
+
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -9,17 +9,26 @@ public class MainForm extends JFrame {
     private JPanel mainPanel;
     private JButton gerirFuncionariosButton;
     private JButton gerirLivrosButton;
+    private JButton gerirSociosButton;
+    private JButton listaSociosButton;
+    private JButton pesquisaSocioButton; // Botão para abrir o PesquisaSocioForm
 
     public MainForm() {
         // Inicialize os componentes
         mainPanel = new JPanel();
         gerirFuncionariosButton = new JButton("Gerir Funcionários");
         gerirLivrosButton = new JButton("Gerir Livros");
+        gerirSociosButton = new JButton("Novo Sócio");
+        listaSociosButton = new JButton("Lista de Sócios");
+        pesquisaSocioButton = new JButton("Pesquisar Sócio"); // Botão para abrir o PesquisaSocioForm
 
         // Configure o layout do painel
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
         mainPanel.add(gerirFuncionariosButton);
         mainPanel.add(gerirLivrosButton);
+        mainPanel.add(gerirSociosButton);
+        mainPanel.add(listaSociosButton);
+        mainPanel.add(pesquisaSocioButton); // Adicionar o botão de pesquisa de sócio
 
         // Configure o JFrame
         setContentPane(mainPanel);
@@ -44,6 +53,32 @@ public class MainForm extends JFrame {
                 livroForm.setVisible(true);
             }
         });
+
+        gerirSociosButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                NovoSocioForm novoSocioForm = new NovoSocioForm();
+                novoSocioForm.setVisible(true);
+            }
+        });
+
+        listaSociosButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ListaSocioForm listaSocioForm = new ListaSocioForm();
+                listaSocioForm.setVisible(true);
+            }
+        });
+
+        pesquisaSocioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                PesquisarSocioForm pesquisaSocioForm = new PesquisarSocioForm();
+                pesquisaSocioForm.setVisible(true);
+            }
+        });
+
+        setVisible(true);
     }
 
     public static void main(String[] args) {
